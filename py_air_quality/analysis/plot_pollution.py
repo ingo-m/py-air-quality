e97188a9f1ea48708ad8a3b2875a1b691d4aee5c
Ingo Marquardt <ingo.marquardt@gmx.de>
2021-04-18 14:23:29 +0200

    Create pollution plots, separately for weekend and weekdays.

--- py_air_quality/analysis/plot_pollution.py
+++ py_air_quality/analysis/plot_pollution.py
@@ -12,9 +12,9 @@ from datetime import datetime
 # ------------------------------------------------------------------------------
 # *** Define parameters
 
-path_csv = 'baseline_measurement.csv'
+path_csv = '/home/john/PhD/GitHub/py-air-quality/py_air_quality/data/baseline_measurement.csv'
 
-path_plot = 'baseline.png'
+path_plot = '/media/ssd_dropbox/Dropbox/Raspberry_Pi/air_pollution_plots/baseline_{}.png'
 
 
 # ------------------------------------------------------------------------------
@@ -53,18 +53,65 @@ daytime = np.add(hour, np.divide(minute, 60.0))
 daytime = np.around(daytime, decimals=3)
 df['daytime'] = daytime
 
+# Currently, there is one row per measurement, with separate columns for the two
+# y variables. We need to change this to 'long format', with one row per
+# datapoint.
+df_pm25 = df[['daytime', 'weekend', 'pm25']]
+df_pm10 = df[['daytime', 'weekend', 'pm10']]
+df_pm25 = df_pm25.rename(columns={'pm25': 'pollution'})
+df_pm10 = df_pm25.rename(columns={'pm10': 'pollution'})
+df_pm25['type'] = 'pm25'
+df_pm10['type'] = 'pm10'
+df = pd.concat([df_pm25, df_pm10], axis=0, ignore_index=True)
+
+
 # ------------------------------------------------------------------------------
-# *** Create plot
+# *** Create plots
 
-# TODO pm25 and pm10 in one plot, separate plots for weekday / weekend
+df_weekend = df.loc[df['weekend'] == True]
+df_weekday = df.loc[df['weekend'] == False]
 
-plot = sns.lineplot(
-    x='daytime',
-    y='pm10',
-    hue='weekend',
-    # style="event",
-    data=df,
-    )
+dict_plot = {'weekend': df_weekend,
+             'weekday': df_weekday,
+             }
 
+# Create separate plots for weekdays (Monday to Friday) and weekend (Saturday
+# and Sunday), which will be saved in separate figures.
+for plot_name, df_plot in dict_plot.items():
 
-df.dtypes
+    graph = sns.lineplot(
+        x='daytime',
+        y='pollution',
+        hue='type',
+        data=df_plot,
+        )
+
+    graph.axes.set_xlim(-0.5, 24.5)
+    graph.axes.set_ylim(0.0, 21.0)
+
+    # Axis layout:
+    graph.axes.set_xlabel('Time [hour]', fontsize=14)
+    graph.axes.set_ylabel('Pollutant concentration [μg/m3]', fontsize=14)
+    graph.axes.set_xticks([0.0, 6.0, 12.0, 18.0, 24.0])
+    graph.axes.tick_params(labelsize=14)
+    graph.axes.spines['top'].set_visible(False)
+    graph.axes.spines['right'].set_visible(False)
+
+    # Adjust legend:
+    graph.legend_.set_title(None)
+    graph.legend_.set_frame_on(False)
+    for i in range(2):
+        legend_text = graph.legend_.texts[i].get_text()
+        if legend_text == 'pm25':
+            graph.legend_.texts[i].set_text('$PM_{2.5}$')
+        elif legend_text == 'pm10':
+            graph.legend_.texts[i].set_text('$PM_{10}$')
+
+    # Save figure:
+    figure = graph.get_figure()
+    figure.savefig(path_plot.format(plot_name),
+                   dpi=160.0,
+                   bbox_inches='tight',
+                   )
+
+    figure.clf()
