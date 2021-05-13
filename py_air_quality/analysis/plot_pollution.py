ce15195604f8e8e4afea0c0168438e142d576f2d
Ingo Marquardt <ingo.marquardt@gmx.de>
2021-05-13 16:43:46 +0200

    Modular function for reading of (indoor) csv measurement data.

--- py_air_quality/analysis/plot_pollution.py
+++ py_air_quality/analysis/plot_pollution.py
@@ -24,38 +24,51 @@ path_plot = '/home/pi/air_quality/baseline_{}.png'
 # ------------------------------------------------------------------------------
 # *** Read & preprocess data
 
+
+def read_csv_data(path_csv):
+
+    df = pd.read_csv(path_csv)
+
+    df = df.replace(to_replace='None', value=np.nan)
+
+    df = df.astype({'pm25': np.float32, 'pm10': np.float32})
+
+    df['datetime'] = [
+        datetime.fromtimestamp(x) for x in df['timestamp'].tolist()
+        ]
+
+    # Convert time from UTC to local time zone:
+    from_zone = tz.tzutc()
+
+    # Tell the datetime object that it's in UTC time zone:
+    df['datetime'] = [
+        x.replace(tzinfo=from_zone) for x in df['datetime'].tolist()
+        ]
+
+    # Convert datetime to local time:
+    df['datetime'] = [
+        x.astimezone(tz.tzlocal()) for x in df['datetime'].tolist()
+        ]
+
+    # Add column for hour of the day:
+
+    # Add column for weekday (where Monday is 0 and Sunday is 6):
+    df['weekday'] = [x.weekday() for x in df['datetime'].tolist()]
+
+    # Add column for weekend (binary; 0 = weekday, 1 = weekend):
+    df['weekend'] = [(5 <= x) for x in df['weekday'].tolist()]
+
+    return df
+
+
 # Wait for the measurement to finish (assuming that the measurement is done at
 # the same frequency, through cron tab).
 sleep(60)
 
-df = pd.read_csv(path_csv)
-
-df = df.replace(to_replace='None', value=np.nan)
-
-df = df.astype({'pm25': np.float32, 'pm10': np.float32})
-
-df['datetime'] = [datetime.fromtimestamp(x) for x in df['timestamp'].tolist()]
-
-# Convert time from UTC to local time zone:
-from_zone = tz.tzutc()
-to_zone = tz.tzlocal()
-
-# Tell the datetime object that it's in UTC time zone:
-df['datetime'] = [x.replace(tzinfo=from_zone) for x in df['datetime'].tolist()]
-
-# Convert datetime to local time:
-df['datetime'] = [x.astimezone(to_zone) for x in df['datetime'].tolist()]
-
-# Add column for hour of the day:
-
-# Add column for weekday (where Monday is 0 and Sunday is 6):
-df['weekday'] = [x.weekday() for x in df['datetime'].tolist()]
-
-# Add column for weekend (binary; 0 = weekday, 1 = weekend):
-df['weekend'] = [(5 <= x) for x in df['weekday'].tolist()]
+df = read_csv_data(path_csv)
 
 # Current, local time:
-local_now = datetime.now(tz=to_zone)
+local_now = datetime.now(tz=tz.tzlocal())
 local_now_hour = (
     float(local_now.hour)
     + (float(local_now.minute) / 60.0)
@@ -89,9 +102,9 @@ df = pd.concat([df_pm10, df_pm25], axis=0, ignore_index=True)
 # ------------------------------------------------------------------------------
 # *** Create plots
 
-df_weekend = df.loc[df['weekend'] == True]
-df_weekday = df.loc[df['weekend'] == False]
-df_last_24_h = df.loc[df['last_24_h'] == True]
+df_weekend = df.loc[df['weekend'] is True]
+df_weekday = df.loc[df['weekend'] is False]
+df_last_24_h = df.loc[df['last_24_h'] is True]
 
 dict_plot = {'last_24_h': df_last_24_h,
              'weekday': df_weekday,
