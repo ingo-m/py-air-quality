916b80acd5c7f9f12aba5146d2279b4ee33fc758
Ingo Marquardt <ingo.marquardt@gmx.de>
2021-05-15 19:28:12 +0200

    WIP: Boxplot internal & external pm2.5 with / without filter.

--- py_air_quality/analysis/preprocessing.py
+++ py_air_quality/analysis/preprocessing.py
@@ -10,8 +10,11 @@ regression analysis in R.
 
 """
 
+import os
+
 import pandas as pd
-from datetime import datetime
+import seaborn as sns
+from datetime import datetime, timedelta, time
 
 from read_csv_data import read_csv_data
 
@@ -29,8 +32,8 @@ path_csv_indoor_filter = '/home/john/PhD/GitHub/py-air-quality/py_air_quality/da
 # https://aqicn.org/data-platform/covid19/
 path_csv_external = '/media/ssd_dropbox/Dropbox/Raspberry_Pi/air_quality_external_data/waqi-covid19-airqualitydata-2020.csv'
 
-# Output path of preprocessed csv file (to be imported into R for analysis):
-path_csv_out = '/home/john/PhD/GitHub/py-air-quality/py_air_quality/data/preprocessed.csv'
+# Output directory for processed csv files (to be imported into R for analysis):
+path_csv_out = '/home/john/PhD/GitHub/py-air-quality/py_air_quality/data/'
 
 # Which pollutant to analyse ('pm25' or 'pm10'):
 pollutant = 'pm25'
@@ -52,12 +55,26 @@ date_format = '%Y-%m-%d'
 df_external['Date'] = \
     [datetime.strptime(x, date_format) for x in df_external['Date'].tolist()]
 
+# The data from aqicn.org has one data point per day. The datetime defaults to
+# midnight (e.g. `2021-03-26 00:00:00`), but the corresponding measurement is
+# average for the entrie day (and the original data is without time, e.g.
+# '2021-03-26'). Add 12 hours, so that the epoch timestamp will correspond to
+# the (temporal) centre of the measurement period.
+df_external['Date'] = \
+    [(x + timedelta(hours=12.0)) for x in df_external['Date'].tolist()]
+
 # Datetime comparisons are error-prone with pandas; rather use epoch timestamp.
 df_external['timestamp'] = \
     [round(x.timestamp()) for x in df_external['Date'].tolist()]
 
-df_external = df_external[['timestamp', 'median']]
-df_external = df_external.rename(columns={'median': (pollutant + '_external')})
+df_external = df_external[['Date', 'timestamp', 'median']]
+df_external = df_external.rename(
+    columns={'median': (pollutant + '_external'),
+             'Date': 'date'}
+    )
+
+# Remove hours, minutes, and seconds from date:
+df_external['date'] = [x.date() for x in df_external['date'].tolist()]
 
 
 # ------------------------------------------------------------------------------
@@ -72,8 +89,86 @@ df_filter = read_csv_data(path_csv_indoor_filter)
 df_baseline['filter'] = False
 df_filter['filter'] = True
 df_internal = pd.concat([df_baseline, df_filter], axis=0, ignore_index=True)
-df_internal = df_internal[['timestamp', pollutant, 'filter', 'weekend']]
+
+df_internal = df_internal[['timestamp', 'datetime', pollutant, 'filter', 'weekend']]
 df_internal = df_internal.rename(columns={pollutant: (pollutant + '_internal')})
+
+
+# ------------------------------------------------------------------------------
+# ***
+
+# Get date without hours, minutes, and seconds, so that we can calculate mean
+# pollutant concentration per day:
+df_internal['date'] = [x.date() for x in df_internal['datetime'].tolist()]
+
+# The external data (from aqicn.org) only has a temporal resolution of one
+# measurement per day (median pollutant concentration per day). For comparison,
+# take the mean across each day for the internal data.
+df_internal_daily = df_internal.groupby(['date']).mean()
+df_internal_daily = df_internal_daily.reset_index(level=0, drop=False)
+
+# Remove day where condition changed from no filter to filter:
+df_internal_daily = \
+    df_internal_daily.loc[df_internal_daily['filter'].isin([0.0, 1.0])]
+
+# Set the epoch timestamp to noon of the corresponding day, so as to be able to
+# match internal and external data:
+df_internal_daily['date'] = \
+    [datetime.combine(x, time(12)) for x in df_internal_daily['date'].tolist()]
+df_internal_daily['timestamp'] = \
+    [round(x.timestamp()) for x in df_internal_daily['date'].tolist()]
+
+# Merge internal and external data (with daily temporal resolution):
+df_internal_daily = df_internal_daily.sort_values('timestamp')
+df_external = df_external.sort_values('timestamp')
+df_daily = pd.merge_asof(
+    df_internal_daily,
+    df_external[['timestamp', 'pm25_external']],
+    on='timestamp',
+    direction='nearest',
+    )
+
+# External and internal data are not on the same scale; divide each by its
+# maximum.
+df_daily['pm25_external'] = (
+    df_daily['pm25_external']
+    / df_daily.loc[df_daily['filter'] == 0.0]['pm25_external'].mean()
+    )
+
+df_daily['pm25_internal'] = (
+    df_daily['pm25_internal']
+    / df_daily.loc[df_daily['filter'] == 0.0]['pm25_internal'].mean()
+    )
+
+# Wide to long format:
+
+df_tmp_ext = \
+    df_daily[['date', 'timestamp', 'pm25_external', 'filter', 'weekend']]
+df_tmp_ext = df_tmp_ext.rename(columns={'pm25_external': pollutant})
+df_tmp_ext['source'] = 'external'
+
+df_tmp_int = \
+    df_daily[['date', 'timestamp', 'pm25_internal', 'filter', 'weekend']]
+df_tmp_int = df_tmp_int.rename(columns={'pm25_internal': pollutant})
+df_tmp_int['source'] = 'internal'
+
+df_daily = pd.concat([df_tmp_ext, df_tmp_int], axis=0, ignore_index=True)
+
+
+graph = sns.catplot(
+    data=df_daily,
+    kind='bar',
+    x='source',
+    y='pm25',
+    hue='filter',
+    ci='sd',
+    #palette="dark",
+    #alpha=.6,
+    #height=6,
+    )
+
+
+
 
 
 # ------------------------------------------------------------------------------
@@ -96,4 +191,10 @@ df = pd.merge_asof(
 df = df[df['pm25_internal'].notna()]
 df = df[df['pm25_external'].notna()]
 
-df.to_csv(path_csv_out, sep=';', index=False)
+df.to_csv(
+    os.path.join(path_csv_out, 'preprocessed.csv'),
+    sep=';',
+    index=False,
+    )
+
+
