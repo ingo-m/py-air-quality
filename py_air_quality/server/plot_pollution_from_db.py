b3438d2e44b846e87ce69dc4422a1a570c8625f4
Ingo Marquardt <ingo.marquardt@gmx.de>
2021-12-29 00:25:32 +0100

    Adjusted conditions. Black code style.

--- py_air_quality/server/plot_pollution_from_db.py
+++ py_air_quality/server/plot_pollution_from_db.py
@@ -9,16 +9,14 @@ Can be run as a cron job (e.g. on remote server):
 
 """
 
+from datetime import datetime, timedelta, timezone
 from time import sleep
 
 import pandas as pd
 import pymongo
-from datetime import datetime, timedelta, timezone
 from dateutil import tz
-
 from py_air_quality.internal.credentials import credentials
 from py_air_quality.server.plot import plot_pollution
-
 
 # ------------------------------------------------------------------------------
 # *** Define parameters
@@ -26,34 +24,38 @@ from py_air_quality.server.plot import plot_pollution
 # Combinations of experimental condition, e.g. 'baseline' or 'with_filter',
 # measurement locations, and sensor types.
 combinations = [
-    {'measurement_location': 'Berlin Kreuzberg',
-     'experimental_condition': 'with_filter',  # 'holiday_without_filter',
-     'sensor_type': 'Nova Fitness SDS011',
-     },
-    {'measurement_location': 'Berlin Kreuzberg',
-     'experimental_condition': 'outdoors_front',
-     'sensor_type': 'Nova Fitness SDS011',
-     },
-    {'measurement_location': 'Alfeld',
-     'experimental_condition': 'outdoors_terrace',
-     'sensor_type': 'Nova Fitness SDS011',
-     },
-    {'measurement_location': 'Leipzig',
-     'experimental_condition': 'outdoors_air_duct',
-     'sensor_type': 'Nova Fitness SDS011',
-     },
-    ]
+    {
+        "measurement_location": "Berlin Kreuzberg",
+        "experimental_condition": "with_filter",  # 'holiday_without_filter',
+        "sensor_type": "Nova Fitness SDS011",
+    },
+    {
+        "measurement_location": "Berlin Kreuzberg",
+        "experimental_condition": "outdoors_front",
+        "sensor_type": "Nova Fitness SDS011",
+    },
+    {
+        "measurement_location": "Alfeld",
+        "experimental_condition": "outdoors_terrace",
+        "sensor_type": "Nova Fitness SDS011",
+    },
+    {
+        "measurement_location": "Alfeld Wohnzimmer",
+        "experimental_condition": "baseline",
+        "sensor_type": "Nova Fitness SDS011",
+    },
+]
 
 # Output path for plots (measurement location, experimental condition and plot
 # name left open;  plot name will be filled in by the child plotting function,
 # e.g. 'last_24h'):
-path_plot = '/home/john/website/site/air_quality_live_data/{}_{}_{}.png'
+path_plot = "/home/john/website/site/air_quality_live_data/{}_{}_{}.png"
 
 # When querying data for aggregate plots, include data from last x days:
 last_x_days = 365
 
 # Time zone used for plots:
-local_time_zone_name = 'Europe/Berlin'
+local_time_zone_name = "Europe/Berlin"
 
 
 # ------------------------------------------------------------------------------
@@ -80,36 +82,34 @@ utc_now = datetime.now(timezone.utc)
 local_time_zone = tz.gettz(local_time_zone_name)
 local_now = utc_now.astimezone(local_time_zone)
 
-local_now_hour = (
-    float(local_now.hour)
-    + (float(local_now.minute) / 60.0)
-    )
+local_now_hour = float(local_now.hour) + (float(local_now.minute) / 60.0)
 
 start_epoch = int(round((utc_now - timedelta(days=last_x_days)).timestamp()))
 
-with pymongo.MongoClient(mongodb_url,
-                         username=mongodb_username,
-                         authMechanism='MONGODB-X509',
-                         tls=True,
-                         tlsCertificateKeyFile=mongodb_tsl_cert,
-                         ) as client:
+with pymongo.MongoClient(
+    mongodb_url,
+    username=mongodb_username,
+    authMechanism="MONGODB-X509",
+    tls=True,
+    tlsCertificateKeyFile=mongodb_tsl_cert,
+) as client:
 
     db = client.air_quality
 
-    db_collection = db['air_quality']
+    db_collection = db["air_quality"]
 
     for combination in combinations:
 
-        measurement_location = combination['measurement_location']
-        experimental_condition = combination['experimental_condition']
-        sensor_type = combination['sensor_type']
+        measurement_location = combination["measurement_location"]
+        experimental_condition = combination["experimental_condition"]
+        sensor_type = combination["sensor_type"]
 
         dict_search = {
-            'timestamp': {'$gt': start_epoch},
-            'experimental_condition': experimental_condition,
-            'measurement_location': measurement_location,
-            'sensor_type': sensor_type,
-            }
+            "timestamp": {"$gt": start_epoch},
+            "experimental_condition": experimental_condition,
+            "measurement_location": measurement_location,
+            "sensor_type": sensor_type,
+        }
 
         search_results = db_collection.find(dict_search)
 
@@ -122,37 +122,32 @@ with pymongo.MongoClient(mongodb_url,
 
         # 'datetime' and 'timestamp' from database should match, but use epoch
         # timestamp as single source of truth and apply time zone conversion.
-        df = df[['timestamp', 'pm25', 'pm10']]
+        df = df[["timestamp", "pm25", "pm10"]]
 
-        df['datetime'] = [
-            datetime.fromtimestamp(x, tz=timezone.utc) for x in
-            df['timestamp'].tolist()
-            ]
+        df["datetime"] = [
+            datetime.fromtimestamp(x, tz=timezone.utc) for x in df["timestamp"].tolist()
+        ]
 
         # Convert datetime to local time:
-        df['datetime'] = [
-            x.astimezone(local_time_zone) for x in
-            df['datetime'].tolist()
-            ]
+        df["datetime"] = [
+            x.astimezone(local_time_zone) for x in df["datetime"].tolist()
+        ]
 
         # Add column for hour of the day:
 
         # Add column for weekday (where Monday is 0 and Sunday is 6):
-        df['weekday'] = [x.weekday() for x in df['datetime'].tolist()]
+        df["weekday"] = [x.weekday() for x in df["datetime"].tolist()]
 
         # Add column for weekend (binary; 0 = weekday, 1 = weekend):
-        df['weekend'] = [(5 <= x) for x in df['weekday'].tolist()]
+        df["weekend"] = [(5 <= x) for x in df["weekday"].tolist()]
 
         # Fill in measurement location and experimental condition into plot
         # name, but leave name for time condition (e.g. 'last_24h') open.
         path_tmp = path_plot.format(
-            measurement_location.replace(' ', '_').lower(),
-            experimental_condition,
-            '{}'
-            )
+            measurement_location.replace(" ", "_").lower(), experimental_condition, "{}"
+        )
 
         # Create plots:
-        plot_pollution(df=df,
-                       utc_now=utc_now,
-                       local_now_hour=local_now_hour,
-                       path_plot=path_tmp)
+        plot_pollution(
+            df=df, utc_now=utc_now, local_now_hour=local_now_hour, path_plot=path_tmp
+        )
